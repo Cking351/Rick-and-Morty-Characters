09c6be170bec96d6288df4daf3bfc04555bbcbac
Chris King <kingchris7777@yahoo.com>
2021-04-27 20:14:51 -0400

    testing

--- rickandmorty/src/App.js
+++ rickandmorty/src/App.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import JumboCard from './Components/JumboCard';
-import Character from './Components/Character';
+import JumboCard from "./Components/JumboCard";
+import Character from "./Components/Character";
 import axios from "axios";
-import logo from './images/logo.png';
+import logo from "./images/logo.png";
 
 const App = () => {
-  const [picklerick, setPicklerick] = useState([]);
+	const [picklerick, setPicklerick] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`https://rickandmortyapi.com/api/character`)
-      .then(response => setPicklerick(response.data.results))
-      .catch(error => error);
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`https://rickandmortyapi.com/api/character`)
+			.then((response) => setPicklerick(response.data.results))
+			.catch((error) => error);
+	}, []);
 
-  return (
-    <div className='App'>
-      <div>
-        <img src={logo} />
-      </div>
-      <h1>Characters</h1>
-      <JumboCard>
-        {picklerick.map(item => (
-          <Character cards={item} />
-        ))}
-      </JumboCard>
-    </div>
-  )
+	return (
+		<div className="App">
+			<div>
+				<img src={logo} />
+			</div>
+			<h1>Characters</h1>
+			<JumboCard>
+				{picklerick.map((item) => (
+					<Character cards={item} />
+				))}
+			</JumboCard>
+		</div>
+	);
 };
 
 export default App;
